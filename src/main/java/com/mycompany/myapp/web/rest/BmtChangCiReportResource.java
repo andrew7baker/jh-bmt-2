b4d4db99b08e0dc85e84027d12754cfa7564982d
baker <gitea@fake.local>
2019-12-04 17:42:18 +0800

    JACKson 生成 json格式 并保存为文件的例子

--- src/main/java/com/mycompany/myapp/web/rest/BmtChangCiReportResource.java
+++ src/main/java/com/mycompany/myapp/web/rest/BmtChangCiReportResource.java
@@ -23,8 +23,44 @@ public class BmtChangCiReportResource {
     @PostMapping("/bmt-report-resource")
     public String bmtReportResource() {
         Jdbi jdbi = Jdbi.create("jdbc:mysql://localhost:3306/bmt?useUnicode=true&characterEncoding=utf8&useSSL=false&useLegacyDatetimeCode=false&serverTimezone=UTC", "root", "");
+        log.info("jdbi"+jdbi);
 
+        //https://howtodoinjava.com/library/json-simple-read-write-json-examples/
 
+        //https://www.programcreek.com/java-api-examples/index.php?api=com.fasterxml.jackson.core.JsonProcessingException
+
+        //First Employee
+        JSONObject employeeDetails = new JSONObject();
+        employeeDetails.put("firstName", "Lokesh");
+        employeeDetails.put("lastName", "Gupta");
+        employeeDetails.put("website", "howtodoinjava.com");
+
+        JSONObject employeeObject = new JSONObject();
+        employeeObject.put("employee", employeeDetails);
+
+        //Second Employee
+        JSONObject employeeDetails2 = new JSONObject();
+        employeeDetails2.put("firstName", "Brian");
+        employeeDetails2.put("lastName", "Schultz");
+        employeeDetails2.put("website", "example.com");
+
+        JSONObject employeeObject2 = new JSONObject();
+        employeeObject2.put("employee", employeeDetails2);
+
+        //Add employees to list
+        JSONArray employeeList = new JSONArray();
+        employeeList.add(employeeObject);
+        employeeList.add(employeeObject2);
+
+        //Write JSON file
+        try (FileWriter file = new FileWriter("employees.json")) {
+
+            file.write(employeeList.toJSONString());
+            file.flush();
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
         return "bmtReportResource";
     }
 
