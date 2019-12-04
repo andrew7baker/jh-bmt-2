0a75a8d08db4b441d24010fbf4e7ee746a657c4e
andrew <andrew@example.com>
2019-12-04 23:53:07 +0800

    report

--- src/main/java/com/mycompany/myapp/web/rest/BmtChangCiReportResource.java
+++ src/main/java/com/mycompany/myapp/web/rest/BmtChangCiReportResource.java
@@ -1,11 +1,15 @@
 package com.mycompany.myapp.web.rest;
 
+import com.alibaba.fastjson.JSONArray;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.jdbi.v3.core.Jdbi;
+
+import java.io.FileWriter;
+import java.io.IOException;
 
 
 /**
@@ -31,33 +35,48 @@ public class BmtChangCiReportResource {
 
         //https://www.programcreek.com/java-api-examples/?api=com.alibaba.fastjson.JSON
 
-        //First Employee
-        JSONObject employeeDetails = new JSONObject();
-        employeeDetails.put("firstName", "Lokesh");
-        employeeDetails.put("lastName", "Gupta");
-        employeeDetails.put("website", "howtodoinjava.com");
+        //https://www.cnblogs.com/pcheng/p/6875760.html
 
-        JSONObject employeeObject = new JSONObject();
-        employeeObject.put("employee", employeeDetails);
+        String jsonStr = "[{\"JACKIE_ZHANG\":\"张学友\"},{\"ANDY_LAU\":\"刘德华\"},{\"LIMING\":\"黎明\"},{\"Aaron_Kwok\":\"郭富城\"}]" ;
+        //做5次测试
+        for(int i=0,j=5;i<j;i++)
+        {
+            JSONArray jsonArray = JSONArray.parseArray(jsonStr);
 
-        //Second Employee
-        JSONObject employeeDetails2 = new JSONObject();
-        employeeDetails2.put("firstName", "Brian");
-        employeeDetails2.put("lastName", "Schultz");
-        employeeDetails2.put("website", "example.com");
+            for(int k=0;k<jsonArray.size();k++){
+                System.out.print(jsonArray.get(k) + "\t");
+            }
+            System.out.println();//用来换行
+        }
 
-        JSONObject employeeObject2 = new JSONObject();
-        employeeObject2.put("employee", employeeDetails2);
 
-        //Add employees to list
-        JSONArray employeeList = new JSONArray();
-        employeeList.add(employeeObject);
-        employeeList.add(employeeObject2);
+    //First Employee
+//        JSONObject employeeDetails = new JSONObject();
+//        employeeDetails.put("firstName", "Lokesh");
+//        employeeDetails.put("lastName", "Gupta");
+//        employeeDetails.put("website", "howtodoinjava.com");
+//
+//        JSONObject employeeObject = new JSONObject();
+//        employeeObject.put("employee", employeeDetails);
+//
+//        //Second Employee
+//        JSONObject employeeDetails2 = new JSONObject();
+//        employeeDetails2.put("firstName", "Brian");
+//        employeeDetails2.put("lastName", "Schultz");
+//        employeeDetails2.put("website", "example.com");
+//
+//        JSONObject employeeObject2 = new JSONObject();
+//        employeeObject2.put("employee", employeeDetails2);
+//
+//        //Add employees to list
+//        JSONArray employeeList = new JSONArray();
+//        employeeList.add(employeeObject);
+//        employeeList.add(employeeObject2);
+//
+//        //Write JSON file
+        try (FileWriter file = new FileWriter("payReport.json")) {
 
-        //Write JSON file
-        try (FileWriter file = new FileWriter("employees.json")) {
-
-            file.write(employeeList.toJSONString());
+            file.write("");
             file.flush();
 
         } catch (IOException e) {
