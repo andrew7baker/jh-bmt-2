ba6e54b952f7325c46428163506bac8a7409885e
baker <gitea@fake.local>
2019-12-05 13:47:58 +0800

    FTP 初步

--- src/main/java/com/mycompany/myapp/web/rest/BmtChangCiReportResource.java
+++ src/main/java/com/mycompany/myapp/web/rest/BmtChangCiReportResource.java
@@ -1,6 +1,8 @@
 package com.mycompany.myapp.web.rest;
 
 import com.alibaba.fastjson.JSONArray;
+import org.apache.commons.net.ftp.*;
+import org.jdbi.v3.core.Handle;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -8,8 +10,10 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.jdbi.v3.core.Jdbi;
 
-import java.io.FileWriter;
-import java.io.IOException;
+import java.io.*;
+import java.util.List;
+
+import java.text.MessageFormat;
 
 
 /**
@@ -21,11 +25,14 @@ public class BmtChangCiReportResource {
 
     private final Logger log = LoggerFactory.getLogger(BmtChangCiReportResource.class);
 
+
+
     /**
     * POST bmtReportResource
     */
     @PostMapping("/bmt-report-resource")
     public String bmtReportResource() {
+        long startTimeMillis = System.currentTimeMillis();
         Jdbi jdbi = Jdbi.create("jdbc:mysql://localhost:3306/bmt?useUnicode=true&characterEncoding=utf8&useSSL=false&useLegacyDatetimeCode=false&serverTimezone=UTC", "root", "");
         log.info("jdbi"+jdbi);
 
@@ -46,34 +53,9 @@ public class BmtChangCiReportResource {
             for(int k=0;k<jsonArray.size();k++){
                 System.out.print(jsonArray.get(k) + "\t");
             }
-            System.out.println();//用来换行
+
         }
 
-
-    //First Employee
-//        JSONObject employeeDetails = new JSONObject();
-//        employeeDetails.put("firstName", "Lokesh");
-//        employeeDetails.put("lastName", "Gupta");
-//        employeeDetails.put("website", "howtodoinjava.com");
-//
-//        JSONObject employeeObject = new JSONObject();
-//        employeeObject.put("employee", employeeDetails);
-//
-//        //Second Employee
-//        JSONObject employeeDetails2 = new JSONObject();
-//        employeeDetails2.put("firstName", "Brian");
-//        employeeDetails2.put("lastName", "Schultz");
-//        employeeDetails2.put("website", "example.com");
-//
-//        JSONObject employeeObject2 = new JSONObject();
-//        employeeObject2.put("employee", employeeDetails2);
-//
-//        //Add employees to list
-//        JSONArray employeeList = new JSONArray();
-//        employeeList.add(employeeObject);
-//        employeeList.add(employeeObject2);
-//
-//        //Write JSON file
         try (FileWriter file = new FileWriter("payReport.json")) {
 
             file.write("");
@@ -82,6 +64,87 @@ public class BmtChangCiReportResource {
         } catch (IOException e) {
             e.printStackTrace();
         }
+
+        String FTP_HOST  = "";
+        int FTP_PORT = 21;
+        String FTP_USER  = "your_username";
+        String FTP_PASS = "your_password";
+        String FTP_REMOTE_DIRECTORY = "/2-yimi";
+        String FTP_LOCAL_DOWNLOAD_DIR = ".";
+
+        List<String> list =jdbi.withHandle(handle ->
+            handle.createQuery("select value from SYS_CONFIG where code = ?")
+                .bind(0, "FTP_USER")
+                .mapTo(String.class)
+                .list());
+        log.info("list="+list);
+        if(list.size()>0){
+            FTP_USER  = list.get(0);
+        }
+
+        list =jdbi.withHandle(handle ->
+            handle.createQuery("select value from SYS_CONFIG where code = ?")
+                .bind(0, "FTP_PASS")
+                .mapTo(String.class)
+                .list());
+        log.info("list="+list);
+        if(list.size()>0){
+            FTP_PASS = list.get(0);
+        }
+
+        list =jdbi.withHandle(handle ->
+            handle.createQuery("select value from SYS_CONFIG where code = ?")
+                .bind(0, "FTP_HOST")
+                .mapTo(String.class)
+                .list());
+        log.info("list="+list);
+        if(list.size()>0){
+            FTP_HOST = list.get(0);
+        }
+
+
+//  https://gist.github.com/nabil-hassan/7e7c6700eee628a7c491
+        FTPClient ftp = new FTPClient();
+
+        try {
+            ftp = new FTPHTTPClient(FTP_HOST , FTP_PORT, FTP_USER , FTP_PASS);
+            log.info("ftp="+ftp);
+            ftp.changeWorkingDirectory(FTP_REMOTE_DIRECTORY);
+            if (!FTPReply.isPositiveCompletion(ftp.getReplyCode())) {
+                throw new RuntimeException("Cannot change to FTP directory: " + FTP_REMOTE_DIRECTORY);
+            }
+
+            // Iteratively download all files in the directory.
+            for (FTPFile file : ftp.listFiles()) {
+                System.out.println(MessageFormat.format("Transferring remote file: {0} to local directory: {1}",
+                    file.getName(), FTP_LOCAL_DOWNLOAD_DIR));
+                File target = new File(FTP_LOCAL_DOWNLOAD_DIR + file.getName());
+                OutputStream outputStream = new BufferedOutputStream(new FileOutputStream(target));
+                ftp.retrieveFile(file.getName(), outputStream);
+                outputStream.close();
+
+                if (!FTPReply.isPositiveCompletion(ftp.getReplyCode())) {
+                    System.out.println(MessageFormat.format("Download for file: {0} failed", file.getName()));
+                }
+            }
+        } catch (Exception ex) {
+            System.err.println(ex);
+        }finally {
+            // Calculate time taken
+            long endTimeMillis = System.currentTimeMillis();
+            long totalTimemillis = endTimeMillis - startTimeMillis;
+            System.out.println(MessageFormat.format("Upload process took {0} ms", totalTimemillis));
+            try {
+                if (ftp.isConnected()) {
+                    ftp.logout();
+                    ftp.disconnect();
+                }
+
+            } catch (IOException ex) {
+                ex.printStackTrace();
+            }
+        }
+
         return "bmtReportResource";
     }
 
