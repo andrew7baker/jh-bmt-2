e93c686a194f465537c6c957bf09eb2041503d3f
baker <gitea@fake.local>
2019-12-22 17:48:58 +0800

    fastjson 拼接json

--- src/main/java/com/mycompany/myapp/web/rest/BmtChangCiReportResource.java
+++ src/main/java/com/mycompany/myapp/web/rest/BmtChangCiReportResource.java
@@ -1,18 +1,20 @@
 package com.mycompany.myapp.web.rest;
 
+import com.alibaba.fastjson.JSON;
 import com.alibaba.fastjson.JSONArray;
+import com.mycompany.myapp.util.ResponseJsonUtils;
 import org.apache.commons.net.ftp.*;
 import org.jdbi.v3.core.Handle;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.jdbi.v3.core.Jdbi;
 
 import java.io.*;
-import java.util.Arrays;
-import java.util.List;
+import java.util.*;
 
 import java.text.MessageFormat;
 
@@ -36,14 +38,6 @@ public class BmtChangCiReportResource {
         long startTimeMillis = System.currentTimeMillis();
         Jdbi jdbi = Jdbi.create("jdbc:mysql://localhost:3306/bmt?useUnicode=true&characterEncoding=utf8&useSSL=false&useLegacyDatetimeCode=false&serverTimezone=UTC", "root", "");
         log.info("jdbi"+jdbi);
-
-        //https://howtodoinjava.com/library/json-simple-read-write-json-examples/
-
-        //https://www.programcreek.com/java-api-examples/index.php?api=com.fasterxml.jackson.core.JsonProcessingException
-
-        //https://www.programcreek.com/java-api-examples/?api=com.alibaba.fastjson.JSON
-
-        //https://www.cnblogs.com/pcheng/p/6875760.html
 
         String jsonStr = "[{\"JACKIE_ZHANG\":\"张学友\"},{\"ANDY_LAU\":\"刘德华\"},{\"LIMING\":\"黎明\"},{\"Aaron_Kwok\":\"郭富城\"}]" ;
         //做5次测试
@@ -158,6 +152,30 @@ public class BmtChangCiReportResource {
             }
         }
         return "bmtReportResource";
+    }
+
+    @GetMapping("/bmt-report")
+    public String bmtGetReport() {
+        Map<String, Object> data = new HashMap<String, Object>();
+
+        data.put("date", new Date());
+//        data.put("email", "accountwcx@qq.com");
+//        data.put("age", 30);
+//        data.put("name", "csdn");
+//        data.put("array", new int[]{1,2,3,4});
+        Map<String, Object> data1 = new HashMap<String, Object>();
+        data1.put("changDiName","2号场地周四");
+
+        Map<String, Object> data2 = new HashMap<String, Object>();
+        data2.put("changDiName","5号场地周日");
+
+        data.put("payChangDis",new Object[]{data1,data2});
+
+
+
+        String s = JSON.toJSONString(data);
+
+        return s;
     }
 
 }
